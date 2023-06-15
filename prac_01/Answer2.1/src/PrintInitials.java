09d01a5f7e996f456d347d55edd5bd5e4dd630a6
Chirag Verma <chiragverma0002@gmail.com>
2023-06-16 00:03:27 +1000

    PrintInitials.java done

--- prac_01/Answer2.1/src/PrintInitials.java
+++ prac_01/Answer2.1/src/PrintInitials.java
@@ -1,17 +1,24 @@
 public class PrintInitials {
 
+    /* This program prints my initials (CV) in big letters,
+       where each letter is nine lines tall.
+    */
+
     public static void main(String[] args) {
         System.out.println();
-        System.out.println("            ***********  **              **   ");
-        System.out.println("           **             **            ** ");
-        System.out.println("           **              **          ** ");
-        System.out.println("           **               **        **");
-        System.out.println("           **                **      **");
-        System.out.println("           **                 **    **");
-        System.out.println("           **                  **  **");
-        System.out.println("           **                   ****  ");
-        System.out.println("            **********           **");
+        System.out.println("       *********        **                **");
+        System.out.println("     **         **       **              **");
+        System.out.println("   **                     **            **");
+        System.out.println("  **                       **          **");
+        System.out.println(" **                         **        **");
+        System.out.println(" **                          **      **");
+        System.out.println("  **                          **    **");
+        System.out.println("   **                          **  **");
+        System.out.println("     **         **              ****");
+        System.out.println("       *********                 ** ");
         System.out.println();
-    }  // end main()
+    } // end main()
 
-}  // end class
+} // end class
+
+
